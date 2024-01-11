c0a4e431d62f4afd67eb450121873f24772092d8
CipherPhantom <oyepitan10@gmail.com>
2024-01-11 15:47:37 +0100

    Change Iterator to Iterable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,9 +2,9 @@
 """
 Defines a type-annotated element_length function
 """
-from typing import Iterator, Sequence, List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns a list of tuples"""
     return [(i, len(i)) for i in lst]
