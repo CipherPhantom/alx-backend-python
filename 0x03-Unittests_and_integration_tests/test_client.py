cd2c7bac1767c532bcb35522ab1f3c58322a3a0c
CipherPhantom <oyepitan10@gmail.com>
2024-04-07 21:00:50 +0100

    Fix Error_v1 - Task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python3
 """
-Test Module for client.py
+Test Module for the file client.py
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized
 from client import GithubOrgClient
+from typing import Dict
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """Class for unit testing the class GithubOrgClient"""
 
     @parameterized.expand([
-        ("google"), ("abc")
+        ("google", {"org": "google"}),
+        ("abc", {"org": "abc"})
     ])
     @patch("client.get_json")
-    def test_org(self, org_name: str, mock_get_json):
+    def test_org(
+        self,
+        org_name: str,
+        expected: Dict,
+        mock_get_json: MagicMock
+    ):
         """Testing the org method of GithubOrgClient class"""
-        mock_get_json.return_value = {"org": org_name}
+        mock_get_json.return_value = expected
         client = GithubOrgClient(org_name)
         result = client.org
         mock_get_json.assert_called_once_with(
             client.ORG_URL.format(org=org_name)
         )
-        self.assertEqual(result, {"org": org_name})
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
