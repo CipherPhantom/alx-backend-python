d75c7ccca40682d4c27cd1f5b8b242bc5988d2e7
CipherPhantom <oyepitan10@gmail.com>
2024-04-07 16:39:43 +0100

    Task 3
    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from typing import Mapping, Sequence, Any
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -56,6 +56,30 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Class for unit testing the function utils.memoize"""
+
+    def test_memoize(self):
+        """Mock test for the function"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mock_method:
+            mock_method.return_value = 42
+            test_instance = TestClass()
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+            mock_method.assert_called_once()
+            self.assertEqual(result1, mock_method.return_value)
+            self.assertEqual(result2, mock_method.return_value)
 
 
 if __name__ == "__main__":
